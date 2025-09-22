37743153f2d92433224a139b6c82e487e168078c
Andrew Sheppard <andrewsheppard8@gmail.com>
2025-09-21 20:54:22 -0700

    Uploading route to check folder

--- app.py
+++ app.py
@@ -545,30 +545,38 @@ def download_all():
 # Unzipping to persistent database
 # -------------------------
 
-@app.route("/admin/upload_data", methods=["GET", "POST"])
+# @app.route("/admin/upload_data", methods=["GET", "POST"])
+# @requires_auth
+# def upload_data():
+#     import zipfile
+
+#     if request.method == "POST" and "file" in request.files:
+#         file = request.files["file"]
+#         if file.filename.endswith(".zip"):
+#             # Save to a temporary location
+#             temp_path = os.path.join("/tmp", file.filename)
+#             file.save(temp_path)
+
+#             # Unzip into PERSISTENT_DIR
+#             with zipfile.ZipFile(temp_path, "r") as zip_ref:
+#                 zip_ref.extractall(PERSISTENT_DIR)
+
+#             return "Data uploaded and extracted successfully!"
+
+#     return '''
+#     <form method="POST" enctype="multipart/form-data">
+#         <input type="file" name="file">
+#         <input type="submit" value="Upload ZIP">
+#     </form>
+#     '''
+
+# -------------------------
+# Check database path to ensure pulling from disk
+# -------------------------
+@app.route("/debug/persistent_dir")
 @requires_auth
-def upload_data():
-    import zipfile
-
-    if request.method == "POST" and "file" in request.files:
-        file = request.files["file"]
-        if file.filename.endswith(".zip"):
-            # Save to a temporary location
-            temp_path = os.path.join("/tmp", file.filename)
-            file.save(temp_path)
-
-            # Unzip into PERSISTENT_DIR
-            with zipfile.ZipFile(temp_path, "r") as zip_ref:
-                zip_ref.extractall(PERSISTENT_DIR)
-
-            return "Data uploaded and extracted successfully!"
-
-    return '''
-    <form method="POST" enctype="multipart/form-data">
-        <input type="file" name="file">
-        <input type="submit" value="Upload ZIP">
-    </form>
-    '''
+def debug_persistent_dir():
+    return f"PERSISTENT_DIR = {PERSISTENT_DIR}"
 
 # -------------------------
 # Run the Flask App
